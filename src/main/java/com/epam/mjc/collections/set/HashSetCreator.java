2b6557eb0edcb1888f9a5a265bb72328eab856e3
Denyonepeny <denisshulga1992@gmail.com>
2023-01-17 19:46:38 +0300

    done

--- src/main/java/com/epam/mjc/collections/set/HashSetCreator.java
+++ src/main/java/com/epam/mjc/collections/set/HashSetCreator.java
@@ -2,8 +2,32 @@ package com.epam.mjc.collections.set;
 
 import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class HashSetCreator {
+    private HashSet<Integer> result = new HashSet<>();
     public HashSet<Integer> createHashSet(List<Integer> sourceList) {
+
+
+        for(Integer i : sourceList){
+            if(i % 2 != 0){
+                result.add(i);
+                result.add(i*2);
+            }else {
+                processEvenNumber(i);
+            }
+        }
+
+        return result;
+    }
+
+    private void processEvenNumber(Integer number){
+        result.add(number);
+        number /= 2;
+        if(number % 2 != 0){
+            result.add(number);
+            return;
+        }
+        processEvenNumber(number);
     }
 }
